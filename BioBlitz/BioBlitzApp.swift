60482c2a6bdf46e74684c52ed4674fc7944cfc72
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-23 10:06:07 +0100

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Philipp Sanktjohanser on 23.12.22.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
